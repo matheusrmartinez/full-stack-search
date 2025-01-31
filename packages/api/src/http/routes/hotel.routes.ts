1d8449e430145e335bbe77bea75544dd085a05fc
Matheus Martinez <matheus.rmartinez@gmail.com>
2025-01-31 21:08:19 +0000

    refactor: improve code structure and environment handling
    - Moved MongoDB client initialization logic to a separate file to enhance readability and separation of concerns.
    - Created a separate route for the `/hotels` endpoint and updated the reference in the `index.ts` file.
    - Created `env.ts` to better handle environment variables, using `zod` for schema validation and default values, and centralizing all environment variable logic.

---
+++ packages/api/src/http/routes/hotel.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { getMongoClient } from 'src/db/mongo';
+export const hotelRoutes = Router();
+hotelRoutes.get('/hotels', async (_, res) => {
+  try {
+    const db = getMongoClient().db();
+    const collection = db.collection('hotels');
+    res.send(await collection.find().toArray());
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
---
+++ packages/api/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import { env } from './config/env';
+import { connectToMongoDB } from './db/mongo';
+import { hotelRoutes } from './http/routes/hotel.routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+await connectToMongoDB();
+
+app.use(hotelRoutes);
+
+const PORT = env.PORT;
+app.listen(PORT, () => {
+  console.log(`API Server Started at ${PORT}`);
+});
